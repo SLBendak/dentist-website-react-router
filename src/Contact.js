23b1020b0374a7c321e2eb3ab7eedef39288d858
mac <shebah712@gmail.com>
2020-08-28 11:00:05 -0700

    [contact/procedures] add contact and procedures component

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+    render() {
+        return(
+            <div>
+                <h1>Contact Us</h1>
+                <p>Jerry Crentist</p>
+                <p>Email: beachBum@gmail.com</p>
+                <p>Phone: 772-772-7772</p>
+            </div>
+        )
+    }
+}
+
+export default Contact
